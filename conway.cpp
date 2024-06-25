9d948b707946672e5a8a03365d10af2410842111
delajc19 <joeydlv776@gmail.com>
2024-06-25 12:34:48 -0400

    updated comments

--- conway.cpp
+++ conway.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define N 26
 const char c = (char)254;
 
-
+//Display board
 void showBoard(char b[N][N]){
     printf("-----------------------------------------------------------------------------------------------------------\n");
     for(int i = 0; i < N; i++){
@@ -18,6 +18,7 @@ void showBoard(char b[N][N]){
 
 }
 
+//Count the live neighbors of a cell at board[i][j]
 int countLiveNeighbors(char b[N][N],int i, int j){
     int num = 0;
     
@@ -33,11 +34,17 @@ int countLiveNeighbors(char b[N][N],int i, int j){
     return num;
 }
 
+//Returns true if the cell at board[i][j] is alive. Else, false
 bool isLive(char b[N][N], int i, int j){
     if(b[i][j]==c){return true;}
     else{return false;}
 }
 
+/* Updates the board and population for one generation.
+ * Passes through the board twice:
+    Pass 1: Counts live neighbors for each cell and stores it in table t
+    Pass 2: Updates each cell on the board b based on the c'sgol rules and the corresponding value in t
+ */
 void generate(char b[N][N], int t[N][N], int &pop){
     int n = N;
     for(int i = 1; i<n-1; i++){
@@ -51,15 +58,16 @@ void generate(char b[N][N], int t[N][N], int &pop){
     for(int i = 1; i<n-1; i++){
         for(int j = 1; j<n-1; j++){
             if(isLive(b,i,j)){
-                //rule 1
+                //rule 1: a live cell with < 2 live neighbors will die (underpopulation)
                 if(t[i][j]<2){b[i][j] = ' ';pop--;continue;}
-                //rule 2
+                //rule 2: a live cell with 2 or 3 live neighbors will survive
                 if(t[i][j]==2 || t[i][j]==3){continue;}
-                //rule 3
+                //rule 3: a live cell with > 3 live neighbors will die (overpopulation)
                 if(t[i][j]>3){b[i][j] = ' ';pop--;continue;}
                 
             }
             else{
+                //rule 4: a dead cell with 3 live neighbors will become alive (reproduction)
                 if(t[i][j]==3){b[i][j] = c;pop++;continue;}
             }  
         }
@@ -91,10 +99,12 @@ void setup(char b[N][N]){
 }
 
 
+
+//Driver Code
 int main(){
     //Change the population and the maximum number of generations to simulate
     int population = 5;
-    int maxGen = 10;
+    int maxGen = 100;
 
     char board[N][N] = {' '};
     int ntable[N][N] = {0};
@@ -105,7 +115,7 @@ int main(){
     int gen = 0;
 
     //loop to run simulation
-    while(gen < maxGen){
+    while(gen <= maxGen){
         printf("Population: %d          Generation: %d\n", population, gen);
         showBoard(board);
         generate(board,ntable, population);
